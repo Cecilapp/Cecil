227044b48778154c5eaadddcd67e1c4780f224c5
Narno <arnaud@ligny.org>
2013-09-23 02:35:52 +0200

    Try Twitter Bootstrap as default layout
    (need to internalise CSS et JS files...)

--- phpoole.php
+++ phpoole.php
@@ -333,29 +333,56 @@ function mkLayoutBaseFile($path, $filename) {
     $subdir = 'layouts';
     $content = <<<'EOT'
 <!DOCTYPE html>
-<!--[if IE 8]><html class="no-js lt-ie9" lang="{{ site.language }}"><![endif]-->
-<!--[if gt IE 8]><!--><html class="no-js" lang="{{ site.language }}"><!--<![endif]-->
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width">
-  <meta name="description" content="{{ site.description }}">
-  <title>{{ site.name}} - {{ title }}</title>
-  <style type="text/css">
-    body { font: bold 24px Helvetica, Arial; padding: 15px 20px; color: #ddd; background: #333;}
-    a:link {text-decoration: none; color: #fff;}
-    a:visited {text-decoration: none; color: #fff;}
-    a:active {text-decoration: none; color: #fff;}
-    a:hover {text-decoration: underline; color: #fff;}
-  </style>
-</head>
-<body>
-  <a href="{{ site.base_url}}"><strong>{{ site.name }}</strong></a><br />
-  <em>{{ site.baseline }}</em>
-  <hr />
-  <p>{{ content }}</p>
-  <hr />
-  <p>Powered by <a href="http://narno.org/PHPoole">PHPoole</a>, coded by <a href="{{ author.home }}">{{ author.name }}</a></p>
-</body>
+<html lang="{{ site.language }}">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="{{ site.description }}">
+    <meta name="author" content="{{ author.name }}">
+    <link rel="shortcut icon" href="http://getbootstrap.com/assets/ico/favicon.png">
+    <title>{{ site.name}} - {{ title }}</title>
+    <link href="http://getbootstrap.com/dist/css/bootstrap.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="http://getbootstrap.com/examples/sticky-footer-navbar/sticky-footer-navbar.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="http://getbootstrap.com/assets/js/html5shiv.js"></script>
+      <script src="http://getbootstrap.com/assets/js/respond.min.js"></script>
+    <![endif]-->
+   <body>
+    <div id="wrap">
+      <div class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="{{ site.base_url }}">{{ site.name}}</a>
+          </div>
+          <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="#">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#contact">Contact</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+      <!-- Begin page content -->
+      <div class="container">
+        {{ content }}
+      </div>
+    </div>
+    <div id="footer">
+      <div class="container">
+        <p class="text-muted credit">Powered by <a href="http://narno.org/PHPoole">PHPoole</a>, coded by <a href="{{ author.home }}">{{ author.name }}</a>.</p>
+      </div>
+    </div>
+    <script src="http://getbootstrap.com/assets/js/jquery.js"></script>
+    <script src="http://getbootstrap.com/dist/js/bootstrap.min.js"></script>
+  </body>
 </html>
 EOT;
     try {
