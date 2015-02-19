5f40e36e87e171a2e0c279d29bd5e81ed49e33f3
Arnaud Ligny <arnaud@ligny.org>
2015-02-19 01:18:13 +0100

    WIP

--- bin/phpoole.php
+++ bin/phpoole.php
@@ -11,6 +11,7 @@
 
 error_reporting(E_ALL ^ E_NOTICE);
 date_default_timezone_set("UTC");
+
 $name       = 'PHPoole';
 $version    = '2.0.0-dev';
 $phpVersion = '5.4.0';
@@ -18,6 +19,7 @@ $phpVersion = '5.4.0';
 use Zend\Console\Console;
 use ZF\Console\Application;
 
+// fetch app version from composer file if exists
 if (file_exists(__DIR__ . '/../composer.json')) {
     @$composer = json_decode(file_get_contents(__DIR__ . '/../composer.json'), true);
     if (isset($composer['version'])) {
@@ -25,30 +27,37 @@ if (file_exists(__DIR__ . '/../composer.json')) {
     }
 }
 
+// compare PHP version
 if (version_compare(PHP_VERSION, $phpVersion, '<')) {
     print('PHP 5.4+ required (your version: ' . PHP_VERSION . ')' . "\n");
     exit(2);
 }
 
-if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
-    $loader = include __DIR__ . '/../vendor/autoload.php';
+// autoload from Phar vendor
+if (Phar::running(false) != '') {
+    require_once Phar::running(true) . '/vendor/autoload.php';
+// autoload from local vendor
 } else {
-    echo 'Run the following commands:' . PHP_EOL;
-    if (!file_exists(__DIR__ . '/../composer.json')) {
-        echo 'curl https://raw.github.com/Narno/PHPoole/master/composer.json > composer.json' . PHP_EOL;
+    if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+        require_once __DIR__ . '/../vendor/autoload.php';
+    } else {
+        echo 'Run the following commands:' . PHP_EOL;
+        if (!file_exists(__DIR__ . '/../composer.phar')) {
+            echo 'curl -s http://getcomposer.org/installer | php' . PHP_EOL;
+        }
+        //if (!file_exists(__DIR__ . '/../composer.json')) {
+        //    echo 'curl https://raw.github.com/Narno/PHPoole/master/composer.json > composer.json' . PHP_EOL;
+        //}
+        echo 'php composer.phar require narno/phpoole';
+        echo 'php composer.phar install' . PHP_EOL;
+        exit(2);
     }
-    if (!file_exists(__DIR__ . '/../composer.phar')) {
-        echo 'curl -s http://getcomposer.org/installer | php' . PHP_EOL;
-    }  
-    echo 'php composer.phar install' . PHP_EOL;
-    exit(2);
 }
 
-$routes = include __DIR__ . '/../config/routes.php';
 $application = new Application(
     $name,
     $version,
-    $routes,
+    include __DIR__ . '/../config/routes.php',
     Console::getInstance()
 );
 
