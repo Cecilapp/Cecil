6a6284aa2415ad5260c43513d87fcd02b76b5c39
Narno <arnaud@ligny.org>
2016-07-01 03:17:31 +0200

    Bump version.

---
+++ src/function.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * This file is part of the PHPoole package.
+ *
+ * Copyright (c) Arnaud Ligny <arnaud@ligny.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+if (!function_exists('isPhar')) {
+    function isPhar() {
+        if (!empty(\Phar::running())) {
+            return true;
+        }
+        return false;
+    }
+}
