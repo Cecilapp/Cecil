751049d3fbf4e0fe2028d7cdc8963eddcb4d5ecb
Narno <arnaud@ligny.org>
2016-07-03 01:08:08 +0200

    fixing deploy script.

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 SOURCE_BRANCH="master"
 TARGET_REPO="github.com/PHPoole/phpoole.github.io.git"
 TARGET_BRANCH="master"
-DIST_FILE="dist/phpoole.phar"
+DIST_FILE="phpoole.phar"
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
     echo "Skipping deploy."
@@ -13,13 +13,13 @@ fi
 
 echo "Starting to deploy Phar to gh-pages"
 
-cp -R $DIST_FILE $HOME/$DIST_FILE
+cp "dist/"$DIST_FILE $HOME/$DIST_FILE
 cd $HOME
 git config --global user.name "Travis"
 git config --global user.email "travis@travis-ci.org"
 git clone --quiet --branch=$TARGET_BRANCH https://${GH_TOKEN}@${TARGET_REPO} gh-pages > /dev/null
 cd gh-pages
-cp -Rf $HOME/$DIST_FILE/* .
+cp $HOME/$DIST_FILE $DIST_FILE
 git add -f .
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
 git push -fq origin $TARGET_BRANCH > /dev/null
