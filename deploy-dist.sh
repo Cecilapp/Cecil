51c9616d9e3d8826345d568e593f5b292a177f7a
Arnaud Ligny <arnaud@ligny.org>
2017-02-15 00:55:38 +0100

    Documentation (#55)
    * Create 1.Overview.md
    * Create 2.Content.md
    * Create 3.Layouts.md
    * Create 4.Configuration.md
    * Update and rename deploy.sh to deploy-dist.sh
    * Create deploy-docs.sh
    * Update .travis.yml

--- deploy-dist.sh
+++ deploy-dist.sh
@@ -4,12 +4,10 @@ set -e
 SOURCE_BRANCH="master"
 TARGET_REPO="PHPoole/phpoole.github.io"
 TARGET_BRANCH="source"
+SOURCE_DIST_DIR="dist"
+TARGET_DIST_DIR="static"
 DIST_FILE="phpoole.phar"
 
-#if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-#    echo "Skipping deploy."
-#    exit 0
-#fi
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
     echo "Skipping deploy (PR? $TRAVIS_PULL_REQUEST)."
     exit 0
@@ -17,12 +15,12 @@ fi
 
 echo "Starting to deploy ${DIST_FILE} to ${TARGET_REPO}..."
 
-cp "dist/"$DIST_FILE $HOME/$DIST_FILE
+cp $SOURCE_DIST_DIR/$DIST_FILE $HOME/$DIST_FILE
 cd $HOME
 git config --global user.name "Travis"
 git config --global user.email "contact@travis-ci.org"
-git clone --quiet --branch=$TARGET_BRANCH https://${GH_TOKEN}@github.com/${TARGET_REPO}.git phpoole > /dev/null
-cd phpoole"/static/"
+git clone --quiet --branch=$TARGET_BRANCH https://${GH_TOKEN}@github.com/${TARGET_REPO}.git gh-pages > /dev/null
+cd gh-pages/$TARGET_DIST_DIR/
 cp $HOME/$DIST_FILE $DIST_FILE
 sha1sum $DIST_FILE > $DIST_FILE".version"
 git add -Af .
