f1e67de503e37d8dd6e0eb2b82b1e5baf548ff51
Narno <arnaud@ligny.org>
2017-03-17 16:47:05 +0100

    Fix headers.

--- res/router.php
+++ res/router.php
@@ -31,27 +31,20 @@ if (empty($ext)) {
     $pathname = $path;
 }
 if (file_exists($filename = $_SERVER['DOCUMENT_ROOT'].$pathname)) {
-    //$ext = pathinfo($pathname, PATHINFO_EXTENSION);
+    $ext = pathinfo($pathname, PATHINFO_EXTENSION);
     $mimeshtml = ['xhtml+xml', 'html'];
     $mimestxt  = ['json', 'xml', 'css', 'csv', 'javascript', 'plain', 'text'];
 
     // get file mime type
     $finfo = finfo_open(FILEINFO_MIME_TYPE);
     $mimetype = finfo_file($finfo, $filename);
+    $mime = explode('/', $mimetype)[1];
     finfo_close($finfo);
 
     // manipulate html and plain text file content for local serve
-    $mime = explode('/', $mimetype)[1];
-
-    if (DEBUG) {
-        http_response_code(500);
-        echo $mimetype;
-        return true;
-    }
-
     if (in_array($mime, $mimeshtml) || in_array($mime, $mimestxt)) {
-        $content = file_get_contents($_SERVER['DOCUMENT_ROOT'].$pathname);
-        // html
+        $content = file_get_contents($filename);
+        // html only
         if (in_array($mime, $mimeshtml)) {
             // inject live reload script
             if (file_exists($_SERVER['DOCUMENT_ROOT'].'/../.phpoole/watch.flag')) {
@@ -59,15 +52,19 @@ if (file_exists($filename = $_SERVER['DOCUMENT_ROOT'].$pathname)) {
                 $content = str_replace('</body>', $script."\n".'</body>', $content);
             }
         }
-        // mime text
-        if (in_array($mime, $mimestxt)) {
-            // replace `baseurl` by `http://localhost:8000/`
-            $baseurl = trim(file_get_contents($_SERVER['DOCUMENT_ROOT'].'/../.phpoole/baseurl'));
-            if (false !== strstr($baseurl, 'http') || $baseurl != '/') {
-                $content = str_replace($baseurl, 'http://localhost:8000/', $content);
-            }
+        // replace `baseurl` by `http://localhost:8000/`
+        $baseurl = trim(file_get_contents($_SERVER['DOCUMENT_ROOT'].'/../.phpoole/baseurl'));
+        if (false !== strstr($baseurl, 'http') || $baseurl != '/') {
+            $content = str_replace($baseurl, 'http://localhost:8000/', $content);
         }
+        // return result
         header('Content-Type: '.$mimetype);
+        if ($ext == 'css') {
+            header('Content-Type: text/css');
+        }
+        if ($ext == 'js') {
+            header('Content-Type: application/javascript');
+        }
         echo $content;
 
         return true;
