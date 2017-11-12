312132dba30e43c58b6a88ca3c0e1c2eafa3b873
Arnaud Ligny <arnaud@ligny.org>
2017-11-12 22:49:29 +0100

    Update deploy scripts. (#102)

--- scripts/deploy-docs.sh
+++ scripts/deploy-docs.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 set -e
 
-SOURCE_BRANCH="master"
+# Deploy documentation files to website
+
+SOURCE_DOCS_DIR="docs"
 TARGET_REPO="PHPoole/phpoole.github.io"
 TARGET_BRANCH="source"
-SOURCE_DOCS_DIR="docs"
 TARGET_DOCS_DIR="content/documentation"
 
-if [ $TRAVIS_PHP_VERSION != "5.6" -o "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-    echo "Skipping deploy."
-    exit 0
-fi
-
-echo "Starting to update documentation..."
-
+echo "Starting to update documentation to ${TARGET_REPO}..."
 cp -R $SOURCE_DOCS_DIR $HOME/$SOURCE_DOCS_DIR
+
+# clone target repo
 cd $HOME
 git config --global user.name "Travis"
 git config --global user.email "contact@travis-ci.org"
-git clone --quiet --branch=$TARGET_BRANCH https://${GH_TOKEN}@github.com/${TARGET_REPO}.git gh-pages > /dev/null
-cd gh-pages
+git clone --quiet --branch=$TARGET_BRANCH https://${GH_TOKEN}@github.com/${TARGET_REPO}.git ${TARGET_REPO} > /dev/null
+
+cd $TARGET_REPO
 mkdir -p $TARGET_DOCS_DIR
+# copy documentation dir
 cp -Rf $HOME/$SOURCE_DOCS_DIR/* $TARGET_DOCS_DIR
+
+# commit and push
 git add -f .
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to $TARGET_DOCS_DIR"
 git push -fq origin $TARGET_BRANCH > /dev/null
