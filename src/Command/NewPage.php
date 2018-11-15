3ed1f5852b76a63342ee24be5c1aa6399e425cf3
Arnaud Ligny <arnaud@ligny.org>
2018-11-15 15:57:02 +0100

    It works! \o/

--- src/Command/NewPage.php
+++ src/Command/NewPage.php
@@ -10,6 +10,7 @@
 
 namespace PHPoole\Command;
 
+use Symfony\Component\Process\Process;
 use Zend\Console\Prompt\Confirm;
 
 class NewPage extends AbstractCommand
@@ -29,7 +30,13 @@ class NewPage extends AbstractCommand
         $this->force = $this->getRoute()->getMatchedParam('force', false);
 
         try {
-            // file content
+            // file name (without extension)
+            // todo: should use config array instead of just '.md'
+            if (false !== $extPos = strripos($this->name, '.md')) {
+                $this->name = substr($this->name, 0, $extPos);
+            }
+            // find archetype
+            $section = strstr($this->name, '/', true);
             $fileContent = <<<'EOT'
 ---
 title: '%title%'
@@ -38,20 +45,23 @@ draft: true
 ---
 
 EOT;
-            if (file_exists($archetype = $this->getPath().'/archetypes/default.md')) {
+            if ($section && file_exists($archetype = sprintf('%s/archetypes/%s.md', $this->getPath(), $section))) {
                 $fileContent = file_get_contents($archetype);
+            } else {
+                if (file_exists($archetype = sprintf('%s/archetypes/default.md', $this->getPath()))) {
+                    $fileContent = file_get_contents($archetype);
+                }
             }
-            // file name (without extension)
-            if (false !== $extPos = strripos($this->name, '.md')) {
-                $this->name = substr($this->name, 0, $extPos);
-            }
+            // path
             $fileRelativePath = $this->getPHPoole()->getConfig()->get('content.dir').'/'.$this->name.'.md';
             $filePath = $this->getPath().'/'.$fileRelativePath;
+            // file already exists?
             if ($this->fs->exists($filePath) && !$this->force) {
                 if (!Confirm::prompt('This page already exists. Do you want to override it? [y/n]', 'y', 'n')) {
                     exit(0);
                 }
             }
+            // create new file
             $title = $this->name;
             if (false !== strrchr($this->name, '/')) {
                 $title = substr(strrchr($this->name, '/'), 1);
@@ -60,8 +70,15 @@ EOT;
             $fileContent = str_replace(['%title%', '%date%'], [$title, $date], $fileContent);
             $this->fs->dumpFile($filePath, $fileContent);
             $this->wlDone(sprintf('File "%s" created!', $fileRelativePath));
-            $editor = 'atom';
-            passthru(sprintf('%s %s', $editor, $filePath), $return_var);
+            // open editor?
+            if ($editor = $this->phpoole->getConfig()->get('editor')) {
+                $command = sprintf('%s %s', $editor, $filePath);
+                $process = new Process($command);
+                $process->run();
+                if (!$process->isSuccessful()) {
+                    throw new \Exception(sprintf("Can't open '%s' editor.", $editor));
+                }
+            }
         } catch (\Exception $e) {
             throw new \Exception(sprintf($e->getMessage()));
         }
