7744b46d9858d7e772e9cdd289ae6f2ff5629cee
Arnaud Ligny <arnaud@ligny.org>
2018-11-15 17:43:17 +0100

    [WIP] Archetypes feature. (#178)
    An Archetype is a page model.
    
    When you create a new page with `phpoole newpage` command, you can specify the file path like `blog/new-post`.
    
    In this case, PHPoole will search for a model file in `archetypes` directory and use it if exist.
    
    In our previous example it will search `archetypes/blog.md` file and use its content to create your new page.

--- src/Command/NewPage.php
+++ src/Command/NewPage.php
@@ -10,6 +10,7 @@
 
 namespace PHPoole\Command;
 
+use Symfony\Component\Process\Process;
 use Zend\Console\Prompt\Confirm;
 
 class NewPage extends AbstractCommand
@@ -29,33 +30,80 @@ class NewPage extends AbstractCommand
         $this->force = $this->getRoute()->getMatchedParam('force', false);
 
         try {
-            $fileContent = <<<'EOT'
----
-title: '%s'
-date: '%s'
-draft: true
----
-# New page
-EOT;
+            // file name (without extension)
             if (false !== $extPos = strripos($this->name, '.md')) {
                 $this->name = substr($this->name, 0, $extPos);
             }
+            // path
             $fileRelativePath = $this->getPHPoole()->getConfig()->get('content.dir').'/'.$this->name.'.md';
             $filePath = $this->getPath().'/'.$fileRelativePath;
+
+            // file already exists?
             if ($this->fs->exists($filePath) && !$this->force) {
                 if (!Confirm::prompt('This page already exists. Do you want to override it? [y/n]', 'y', 'n')) {
                     exit(0);
                 }
             }
+
+            // create new file
             $title = $this->name;
             if (false !== strrchr($this->name, '/')) {
                 $title = substr(strrchr($this->name, '/'), 1);
             }
             $date = date('Y-m-d');
-            $this->fs->dumpFile($filePath, sprintf($fileContent, $title, $date));
+            $fileContent = str_replace(['%title%', '%date%'], [$title, $date], $this->findArchetype($this->name));
+            $this->fs->dumpFile($filePath, $fileContent);
             $this->wlDone(sprintf('File "%s" created!', $fileRelativePath));
+            // open editor?
+            $this->openEditor($filePath);
         } catch (\Exception $e) {
             throw new \Exception(sprintf($e->getMessage()));
+        }
+    }
+
+    /**
+     * Find the archtetype and return its content.
+     *
+     * @param string $name
+     *
+     * @return string
+     */
+    protected function findArchetype($name)
+    {
+        $section = strstr($this->name, '/', true);
+        if ($section && file_exists($archetype = sprintf('%s/archetypes/%s.md', $this->getPath(), $section))) {
+            return file_get_contents($archetype);
+        }
+        if (file_exists($archetype = sprintf('%s/archetypes/default.md', $this->getPath()))) {
+            return file_get_contents($archetype);
+        }
+
+        return <<<'EOT'
+---
+title: '%title%'
+date: '%date%'
+draft: true
+---
+
+EOT;
+    }
+
+    /**
+     * Open new file in editor (if configured).
+     *
+     * @param string $filePath
+     *
+     * @return void
+     */
+    protected function openEditor($filePath)
+    {
+        if ($editor = $this->phpoole->getConfig()->get('editor')) {
+            $command = sprintf('%s %s', $editor, $filePath);
+            $process = new Process($command);
+            $process->run();
+            if (!$process->isSuccessful()) {
+                throw new \Exception(sprintf("Can't open '%s' editor.", $editor));
+            }
         }
     }
 }
