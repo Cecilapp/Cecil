c8605fd6034eb4ecd9ffe3e95c847c3fd5995a0a
Arnaud Ligny <arnaud@ligny.org>
2018-11-15 16:35:02 +0100

    Clean code.

--- src/Command/NewPage.php
+++ src/Command/NewPage.php
@@ -34,9 +34,50 @@ class NewPage extends AbstractCommand
             if (false !== $extPos = strripos($this->name, '.md')) {
                 $this->name = substr($this->name, 0, $extPos);
             }
-            // find archetype
-            $section = strstr($this->name, '/', true);
-            $fileContent = <<<'EOT'
+            // path
+            $fileRelativePath = $this->getPHPoole()->getConfig()->get('content.dir').'/'.$this->name.'.md';
+            $filePath = $this->getPath().'/'.$fileRelativePath;
+
+            // file already exists?
+            if ($this->fs->exists($filePath) && !$this->force) {
+                if (!Confirm::prompt('This page already exists. Do you want to override it? [y/n]', 'y', 'n')) {
+                    exit(0);
+                }
+            }
+
+            // create new file
+            $title = $this->name;
+            if (false !== strrchr($this->name, '/')) {
+                $title = substr(strrchr($this->name, '/'), 1);
+            }
+            $date = date('Y-m-d');
+            $fileContent = str_replace(['%title%', '%date%'], [$title, $date], $this->findArchetype($this->name));
+            $this->fs->dumpFile($filePath, $fileContent);
+            $this->wlDone(sprintf('File "%s" created!', $fileRelativePath));
+            // open editor?
+            $this->openEditor($filePath);
+        } catch (\Exception $e) {
+            throw new \Exception(sprintf($e->getMessage()));
+        }
+    }
+
+    /**
+     * Find the archtetype and return its content.
+     *
+     * @param string $name
+     *
+     * @return string
+     */
+    protected function findArchetype($name)
+    {
+        $section = strstr($this->name, '/', true);
+        if ($section && file_exists($archetype = sprintf('%s/archetypes/%s.md', $this->getPath(), $section))) {
+            return file_get_contents($archetype);
+        }
+        if (file_exists($archetype = sprintf('%s/archetypes/default.md', $this->getPath()))) {
+            return file_get_contents($archetype);
+        }
+        return <<<'EOT'
 ---
 title: '%title%'
 date: '%date%'
@@ -44,42 +85,24 @@ draft: true
 ---
 
 EOT;
-            if ($section && file_exists($archetype = sprintf('%s/archetypes/%s.md', $this->getPath(), $section))) {
-                $fileContent = file_get_contents($archetype);
-            } else {
-                if (file_exists($archetype = sprintf('%s/archetypes/default.md', $this->getPath()))) {
-                    $fileContent = file_get_contents($archetype);
-                }
+    }
+
+    /**
+     * Open new file in editor (if configured).
+     *
+     * @param string $filePath
+     *
+     * @return void
+     */
+    protected function openEditor($filePath)
+    {
+        if ($editor = $this->phpoole->getConfig()->get('editor')) {
+            $command = sprintf('%s %s', $editor, $filePath);
+            $process = new Process($command);
+            $process->run();
+            if (!$process->isSuccessful()) {
+                throw new \Exception(sprintf("Can't open '%s' editor.", $editor));
             }
-            // path
-            $fileRelativePath = $this->getPHPoole()->getConfig()->get('content.dir').'/'.$this->name.'.md';
-            $filePath = $this->getPath().'/'.$fileRelativePath;
-            // file already exists?
-            if ($this->fs->exists($filePath) && !$this->force) {
-                if (!Confirm::prompt('This page already exists. Do you want to override it? [y/n]', 'y', 'n')) {
-                    exit(0);
-                }
-            }
-            // create new file
-            $title = $this->name;
-            if (false !== strrchr($this->name, '/')) {
-                $title = substr(strrchr($this->name, '/'), 1);
-            }
-            $date = date('Y-m-d');
-            $fileContent = str_replace(['%title%', '%date%'], [$title, $date], $fileContent);
-            $this->fs->dumpFile($filePath, $fileContent);
-            $this->wlDone(sprintf('File "%s" created!', $fileRelativePath));
-            // open editor?
-            if ($editor = $this->phpoole->getConfig()->get('editor')) {
-                $command = sprintf('%s %s', $editor, $filePath);
-                $process = new Process($command);
-                $process->run();
-                if (!$process->isSuccessful()) {
-                    throw new \Exception(sprintf("Can't open '%s' editor.", $editor));
-                }
-            }
-        } catch (\Exception $e) {
-            throw new \Exception(sprintf($e->getMessage()));
         }
     }
 }
