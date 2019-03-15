a94a3745b3d70542cdacb56d5a7df92ace01c07b
Arnaud Ligny <arnaud@ligny.org>
2019-03-15 15:43:07 +0100

    Clean ressource templates (#294)

--- res/server/livereload.js
+++ res/server/livereload.js
@@ -1,8 +1,10 @@
-<!-- Cecil live reload script -->
-<script>
-var evtSource = new EventSource('http://localhost:8000/watcher');
-evtSource.addEventListener('reload', function(e) {
-  console.log('reload');
-  location.reload(true);
-}, false);
-</script>
+
+    <!-- Cecil: live reload script -->
+    <script>
+    var evtSource = new EventSource('http://localhost:8000/watcher');
+    evtSource.addEventListener('reload', function(e) {
+      console.log('reload');
+      location.reload(true);
+    }, false);
+    </script>
+    <!-- /Cecil -->
--- res/server/router.php
+++ res/server/router.php
@@ -61,7 +61,7 @@ if (file_exists($filename = $_SERVER['DOCUMENT_ROOT'].$pathname)) {
         if (in_array($mime, $mimeshtml)) {
             // inject live reload script
             $script = file_get_contents(__DIR__.'/livereload.js');
-            $content = str_replace('</body>', $script."\n".'</body>', $content);
+            $content = str_replace('</body>', "$script\n  </body>", $content);
         }
         // replace `baseurl` by `http://localhost:8000/`
         $baseurl = trim(file_get_contents($_SERVER['DOCUMENT_ROOT'].'/../'.SERVER_TMP_DIR.'/baseurl'));
