9880c9c0b768e8e5a6242f2367fddc2fedd9efaa
Arnaud Ligny <arnaud@ligny.org>
2019-12-19 17:16:45 +0100

    WIP

---
+++ src/Application.php
@@ -0,0 +1,53 @@
+<?php
+/*
+ * Copyright (c) Arnaud Ligny <arnaud@ligny.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Cecil;
+
+use Symfony\Component\Console\Application as BaseApplication;
+use Symfony\Component\Console\Input\InputArgument;
+
+
+/**
+ * The console application that handles the commands
+ */
+class Application extends BaseApplication
+{
+    private static $banner = '  ____          _ _
+ / ___|___  ___(_) | Your content driven
+| |   / _ \/ __| | | static site generator.
+| |__|  __/ (__| | |
+ \____\___|\___|_|_| by Arnaud Ligny
+';
+
+    public function getHelp()
+    {
+        return self::$banner.parent::getHelp();
+    }
+
+    /**
+     * Initializes all the composer commands.
+     */
+    protected function getDefaultCommands()
+    {
+        $commands = array_merge(parent::getDefaultCommands(), array(
+            new Command\CommandTest(),
+        ));
+        if (Util\Plateform::isPhar()) {
+            $commands[] = new Command\SelfUpdate($this->getVersion());
+        }
+        return $commands;
+    }
+
+    protected function getDefaultInputDefinition()
+    {
+        $definition = parent::getDefaultInputDefinition();
+        $definition->addArgument(new InputArgument('path', InputArgument::OPTIONAL, 'Path to the Website'));
+
+        return $definition;
+    }
+}
---
+++ src/Command/CommandTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Cecil\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CommandTest extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('test')
+            ->setDescription('This is a test.')
+            ->setHelp('This command allows you to do a test...');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('A test! ' . $input->getArgument('path'));
+
+        return 0;
+    }
+}
