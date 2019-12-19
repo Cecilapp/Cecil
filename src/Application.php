5f68e3181e748040014c1afe24e7544cb7c79b99
Arnaud Ligny <arnaud@ligny.org>
2019-12-19 17:17:14 +0100

    Apply fixes from StyleCI (#491)

--- src/Application.php
+++ src/Application.php
@@ -11,9 +11,8 @@ namespace Cecil;
 use Symfony\Component\Console\Application as BaseApplication;
 use Symfony\Component\Console\Input\InputArgument;
 
-
 /**
- * The console application that handles the commands
+ * The console application that handles the commands.
  */
 class Application extends BaseApplication
 {
@@ -34,12 +33,13 @@ class Application extends BaseApplication
      */
     protected function getDefaultCommands()
     {
-        $commands = array_merge(parent::getDefaultCommands(), array(
+        $commands = array_merge(parent::getDefaultCommands(), [
             new Command\CommandTest(),
-        ));
+        ]);
         if (Util\Plateform::isPhar()) {
             $commands[] = new Command\SelfUpdate($this->getVersion());
         }
+
         return $commands;
     }
 
