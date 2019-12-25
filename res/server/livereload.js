c06bcdf03aa812ec628ccede2560e44e598f2c23
Arnaud Ligny <arnaud@ligny.org>
2019-12-25 17:48:45 +0100

    Enhance serve livereload (#498)

--- res/server/livereload.js
+++ res/server/livereload.js
@@ -1,10 +1,16 @@
 
     <!-- Cecil: live reload script -->
     <script>
-    var evtSource = new EventSource('/watcher');
-    evtSource.addEventListener('reload', function(e) {
-      console.log('reload');
-      location.reload(true);
-    }, false);
+      var evtSource = new EventSource('/watcher');
+      evtSource.addEventListener('reload', function(e) {
+        let lastChange = e.data;
+        let lastReload = sessionStorage.getItem('livereload');
+
+        if (lastChange > lastReload) {
+          sessionStorage.setItem('livereload', Math.floor(Date.now() / 1000));
+          console.log('reload now!');
+          location.reload(true);
+        }
+      }, false);
     </script>
     <!-- /Cecil -->
