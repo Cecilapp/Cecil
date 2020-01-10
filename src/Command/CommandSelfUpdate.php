3fbe37f4cfdcbcdfac7216c4a52926ab3a5c1aa1
Arnaud Ligny <arnaud@ligny.org>
2020-01-10 11:50:14 +0100

    Update CommandSelfUpdate.php

--- src/Command/CommandSelfUpdate.php
+++ src/Command/CommandSelfUpdate.php
@@ -12,7 +12,6 @@ use Humbug\SelfUpdate\Updater;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputDefinition;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class CommandSelfUpdate extends Command
