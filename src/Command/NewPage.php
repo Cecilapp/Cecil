a5897105f4885c62c354187b3229001ab6c5667c
Arnaud Ligny <arnaud@ligny.org>
2020-01-11 00:11:23 +0100

    Update NewPage.php

--- src/Command/NewPage.php
+++ src/Command/NewPage.php
@@ -1,5 +1,7 @@
 <?php
 /*
+ * This file is part of the Cecil/Cecil package.
+ *
  * Copyright (c) Arnaud Ligny <arnaud@ligny.org>
  *
  * For the full copyright and license information, please view the LICENSE
