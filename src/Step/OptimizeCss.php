294330f8245cf98867b15e3bc4c42037bd508bc9
Arnaud Ligny <arnaud@ligny.org>
2020-01-27 15:53:59 +0100

    Update OptimizeCss.php

--- src/Step/OptimizeCss.php
+++ src/Step/OptimizeCss.php
@@ -9,99 +9,21 @@
 namespace Cecil\Step;
 
 use MatthiasMullie\Minify;
-use Symfony\Component\Finder\Finder;
 
 /**
  * CSS files Optimization.
  */
-class OptimizeCss extends AbstractStep
+class OptimizeCss extends AbstractStepOptimize
 {
-    const TYPE = 'css';
-
-    /**
-     * {@inheritdoc}
-     */
-    public function init($options)
+    public function setProcessor()
     {
-        if (false === $this->builder->getConfig()->get(sprintf('optimize.%s.enabled', self::TYPE))
-            || false === $this->builder->getConfig()->get('optimize.enabled')) {
-            $this->process = false;
-
-            return;
-        }
-        if ($options['dry-run']) {
-            $this->process = false;
-
-            return;
-        }
-        if (is_dir($this->builder->getConfig()->getOutputPath())) {
-            $this->process = true;
-        }
+        $this->type = 'css';
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function process()
+    public function processFile(\Symfony\Component\Finder\SplFileInfo $file)
     {
-        call_user_func_array($this->builder->getMessageCb(), ['OPTIMIZE', sprintf('Optimizing %s', self::TYPE)]);
-
-        $extensions = $this->builder->getConfig()->get(sprintf('optimize.%s.ext', self::TYPE));
-        $files = Finder::create()
-            ->files()
-            ->in($this->builder->getConfig()->getOutputPath())
-            ->name('/\.('.implode('|', $extensions).')$/')
-            ->notName('/\.min\.('.implode('|', $extensions).')$/')
-            ->sortByName(true);
-        $max = count($files);
-
-        if ($max <= 0) {
-            $message = 'No files';
-            call_user_func_array($this->builder->getMessageCb(), ['OPTIMIZE_PROGRESS', $message]);
-
-            return;
-        }
-
-        $count = 0;
-        $optimized = 0;
-
-        // setup processor
-	    //
-
-        /* @var $file \Symfony\Component\Finder\SplFileInfo */
-        foreach ($files as $file) {
-            $count++;
-
-            $sizeBefore = $file->getSize();
-
-            // process file
-            $minifier = new Minify\CSS($file->getPathname());
-            $minified = $minifier->minify();
-            \Cecil\Util::getFS()->dumpFile($file->getPathname(), $minified);
-
-            $sizeAfter = $file->getSize();
-
-            $subpath = \Cecil\Util::getFS()->makePathRelative(
-                $file->getPath(),
-                $this->builder->getConfig()->getOutputPath()
-            );
-            $subpath = trim($subpath, './');
-            $path = $subpath ? $subpath.'/'.$file->getFilename() : $file->getFilename();
-
-            $message = sprintf(
-                '%s: %s Ko -> %s Ko',
-                $path,
-                ceil($sizeBefore / 1000),
-                ceil($sizeAfter / 1000)
-            );
-            call_user_func_array($this->builder->getMessageCb(), ['OPTIMIZE_PROGRESS', $message, $count, $max]);
-            if ($sizeAfter < $sizeBefore) {
-                $optimized++;
-            }
-        }
-        if ($optimized == 0) {
-            $message = 'Nothing to do';
-            call_user_func_array($this->builder->getMessageCb(), ['OPTIMIZE_PROGRESS', $message]);
-        }
+        $minifier = new Minify\CSS($file->getPathname());
+        $minified = $minifier->minify();
+        \Cecil\Util::getFS()->dumpFile($file->getPathname(), $minified);
     }
 }
