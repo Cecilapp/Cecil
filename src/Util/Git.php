4b317d8b74d97474b67b5b331af7ca2cac39e664
Arnaud Ligny <arnaud@ligny.org>
2020-02-25 11:10:10 +0100

    Cleaning utils

---
+++ src/Util/Git.php
@@ -0,0 +1,45 @@
+<?php
+/*
+ * This file is part of the Cecil/Cecil package.
+ *
+ * Copyright (c) Arnaud Ligny <arnaud@ligny.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Cecil\Util;
+
+use Symfony\Component\Process\Process;
+
+class Git
+{
+    /**
+     * Runs a Git command on the repository.
+     *
+     * @param string $command The command.
+     *
+     * @throws \RuntimeException If the command failed.
+     *
+     * @return string The trimmed output from the command.
+     */
+    public static function runGitCommand(string $command): string
+    {
+        try {
+            $process = new Process($command, __DIR__);
+            if (0 === $process->run()) {
+                return trim($process->getOutput());
+            }
+
+            throw new \RuntimeException(
+                sprintf(
+                    'The tag or commit hash could not be retrieved from "%s": %s',
+                    __DIR__,
+                    $process->getErrorOutput()
+                )
+            );
+        } catch (\RuntimeException $exception) {
+            throw new \RuntimeException('Process error');
+        }
+    }
+}
