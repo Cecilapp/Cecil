c4adb71ffc02bb13d0c9033d15eef929beb6cbbe
Arnaud Ligny <arnaud@ligny.org>
2020-04-11 22:21:02 +0200

    Create Cache.php

---
+++ src/Cache/Cache.php
@@ -0,0 +1,162 @@
+<?php
+/**
+ * This file is part of the Cecil/Cecil package.
+ *
+ * Copyright (c) Arnaud Ligny <arnaud@ligny.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Cecil\Cache;
+
+use Cecil\Builder;
+use Cecil\Config;
+use Cecil\Util;
+
+class Cache
+{
+    /** @var Builder */
+    protected $builder;
+    /** @var Config */
+    protected $config;
+    /** @var string */
+    protected $scope;
+    /** @var string */
+    protected $rootPath;
+
+    /**
+     * @param Builder $builder
+     * @param string $scope
+     * @param string $rootPath
+     */
+    public function __construct(Builder $builder, string $scope, string $rootPath)
+    {
+        $this->builder = $builder;
+        $this->config = $builder->getConfig();
+
+        $this->scope = $scope;
+        $this->rootPath = $rootPath;
+    }
+
+    /**
+     * Returns true if cache entry already exists.
+     *
+     * @param string $file
+     *
+     * @return bool
+     */
+    public function isExists(string $file): bool
+    {
+        if (!Util::getFS()->exists($this->getCachePathname($file)) || !Util::getFS()->exists($this->getHashFile($file))) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Returns MD5 hash of $file.
+     *
+     * @param string $file
+     *
+     * @return string
+     */
+    public function getHash(string $file): string
+    {
+        return hash_file('md5', $file);
+    }
+
+    /**
+     * Copying cached file and creates hash file.
+     *
+     * @param string $file
+     * @param string|null $hash
+     *
+     * @return void
+     */
+    public function save(string $file, string $hash = null): void
+    {
+        $this->removesHashFile($this->getRelativePathname($file));
+        // file
+        Util::getFS()->copy($file, $this->getCachePathname($file), true);
+        // hash
+        Util::getFS()->mkdir(Util::joinFile($this->config->getCachePath(), Util::joinFile($this->scope, 'hash')));
+        Util::getFS()->touch($this->getHashFile($file, $hash));
+    }
+
+    /**
+     * Returns path to the from from the $rootPath.
+     *
+     * @param string $file
+     *
+     * @return string
+     */
+    private function getRelativePathname(string $file): string
+    {
+        $this->relativePath = trim(Util::getFS()->makePathRelative(dirname($file), $this->rootPath), './');
+        return Util::joinFile($this->relativePath, basename($file));
+    }
+
+    /**
+     * Creates cached file path.
+     *
+     * @param string $file
+     *
+     * @return string
+     */
+    private function getCachePathname($file): string
+    {
+        return Util::joinFile(
+            $this->config->getCachePath(),
+            Util::joinFile($this->scope, 'files'),
+            $this->getRelativePathname($file)
+        );
+    }
+
+    /**
+     * Creates file hash.
+     *
+     * @param string $file
+     * @param string|null $hash
+     *
+     * @return string
+     */
+    private function getHashFile(string $file, string $hash = null): string
+    {
+        if ($hash === null) {
+            $hash = $this->getHash($file);
+        }
+        return Util::joinFile(
+            $this->config->getCachePath(),
+            Util::joinFile($this->scope, 'hash'),
+            $this->preparesHashFile($this->getRelativePathname($file)).$hash
+        );
+    }
+
+    /**
+     * Prepares hash file path.
+     *
+     * @param string $path
+     *
+     * @return string
+     */
+    private function preparesHashFile(string $path): string
+    {
+        return str_replace(DIRECTORY_SEPARATOR, '-', $path).'_';
+    }
+    /**
+     * Removes previous hash files.
+     *
+     * @param string $path
+     *
+     * @return void
+     */
+    private function removesHashFile(string $path): void
+    {
+        $pattern = Util::joinFile($this->config->getCachePath(), Util::joinFile($this->scope, 'hash'), $this->preparesHashFile($path)).'*';
+        foreach (glob($pattern) as $filename) {
+            Util::getFS()->remove($filename);
+        }
+    }
+}
