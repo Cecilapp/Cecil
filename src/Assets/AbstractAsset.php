f7d47cd8d05dc470895b97c38edce0b6fb043bc0
Arnaud Ligny <arnaud@ligny.org>
2020-04-12 01:46:31 +0200

    Update AbstractAsset.php

--- src/Assets/AbstractAsset.php
+++ src/Assets/AbstractAsset.php
@@ -23,7 +23,7 @@ abstract class AbstractAsset
     const CACHE_ASSETS_DIR = 'assets';
 
     /**
-     * @var Builder
+     * @param Builder
      */
     public function __construct(Builder $builder)
     {
