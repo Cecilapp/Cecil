d1f348e70217864b94e557088ab3b8df243da588
Arnaud Ligny <arnaud@ligny.org>
2020-04-24 01:09:59 +0200

    Update test config

--- tests/Build.php
+++ tests/Build.php
@@ -16,12 +16,14 @@ use Symfony\Component\Filesystem\Filesystem;
 class Build extends \PHPUnit\Framework\TestCase
 {
     protected $wsSourceDir;
+    protected $config;
     protected $wsDestinationDir;
     const DEBUG = false;
 
     public function setUp()
     {
         $this->wsSourceDir = Util::joinFile(__DIR__, 'fixtures/website');
+        $this->config = Util::joinFile($this->wsSourceDir, 'config.php');
         $this->wsDestinationDir = $this->wsSourceDir;
     }
 
@@ -40,102 +42,7 @@ class Build extends \PHPUnit\Framework\TestCase
         putenv('CECIL_TITLE=Cecil');
         putenv('CECIL_DESCRIPTION=Description (env. variable)');
         Builder::create(
-            [
-                'debug'      => true,
-                'title'      => 'Cecil test',
-                'taxonomies' => [
-                    'tests' => 'disabled',
-                ],
-                'menus' => [
-                    'main' => [
-                        [
-                            'id'   => 'index',
-                            'name' => 'Da home! \o/',
-                        ],
-                        [
-                            'id'      => 'about',
-                            'enabled' => false,
-                        ],
-                        [
-                            'id'     => 'aligny',
-                            'name'   => 'The author',
-                            'url'    => 'https://arnaudligny.fr',
-                            'weight' => 9999,
-                        ],
-                    ],
-                ],
-                'pagination' => [
-                    'enabled' => true,
-                ],
-                'theme' => [
-                    'a-theme',
-                    'hyde',
-                ],
-                'googleanalytics' => 'UA-XXXXX',
-                'output'          => [
-                    'formats' => [
-                        [
-                            'name'      => 'netlify_redirects',
-                            'mediatype' => 'text/plain',
-                            'extension' => '',
-                        ],
-                    ],
-                    'pagetypeformats' => [
-                        'page'       => ['html', 'json'],
-                        'homepage'   => ['html', 'atom', 'rss', 'json'],
-                        'section'    => ['html', 'atom', 'rss', 'json'],
-                        'vocabulary' => ['html'],
-                        'term'       => ['html', 'atom', 'rss'],
-                    ],
-                ],
-                'language'  => 'en',
-                'languages' => [
-                    [
-                        'code'   => 'en',
-                        'name'   => 'English',
-                        'locale' => 'en_US',
-                    ],
-                    [
-                        'code'   => 'fr',
-                        'name'   => 'Français',
-                        'locale' => 'fr_FR',
-                        'config' => [
-                            'title'       => 'Cecil FR',
-                            'description' => 'En français !',
-                        ],
-                    ],
-                ],
-                'virtualpages' => [
-                    [
-                        'path'   => '_redirects',
-                        'output' => 'netlify_redirects',
-                    ],
-                    [
-                        'path'      => 'rss',
-                        'published' => false,
-                    ],
-                ],
-                'defaultpages'    => [
-                    'sitemap' => [
-                        'published' => false,
-                        'priority'  => 99,
-                    ],
-                ],
-                'static' => [
-                    'exclude' => [
-                        'test*.txt',
-                        '/\.php$/',
-                    ],
-                    'load' => true,
-                ],
-                'generators' => [
-                    99  => 'Cecil\Generator\Test',
-                    100 => 'Cecil\Generator\TitleReplace',
-                ],
-                'cache' => [
-                    'enabled' => true,
-                ],
-            ],
+            require($this->config),
             new PrintLogger(Builder::VERBOSITY_DEBUG)
         )->setSourceDir($this->wsSourceDir)
         ->setDestinationDir($this->wsDestinationDir)
---
+++ tests/fixtures/website/config.php
@@ -0,0 +1,97 @@
+<?php
+return [
+    'debug'      => true,
+    'title'      => 'Cecil test',
+    'taxonomies' => [
+        'tests' => 'disabled',
+    ],
+    'menus' => [
+        'main' => [
+            [
+                'id'   => 'index',
+                'name' => 'Da home! \o/',
+            ],
+            [
+                'id'      => 'about',
+                'enabled' => false,
+            ],
+            [
+                'id'     => 'aligny',
+                'name'   => 'The author',
+                'url'    => 'https://arnaudligny.fr',
+                'weight' => 9999,
+            ],
+        ],
+    ],
+    'pagination' => [
+        'enabled' => true,
+    ],
+    'theme' => [
+        'a-theme',
+        'hyde',
+    ],
+    'googleanalytics' => 'UA-XXXXX',
+    'output'          => [
+        'formats' => [
+            [
+                'name'      => 'netlify_redirects',
+                'mediatype' => 'text/plain',
+                'extension' => '',
+            ],
+        ],
+        'pagetypeformats' => [
+            'page'       => ['html', 'json'],
+            'homepage'   => ['html', 'atom', 'rss', 'json'],
+            'section'    => ['html', 'atom', 'rss', 'json'],
+            'vocabulary' => ['html'],
+            'term'       => ['html', 'atom', 'rss'],
+        ],
+    ],
+    'language'  => 'en',
+    'languages' => [
+        [
+            'code'   => 'en',
+            'name'   => 'English',
+            'locale' => 'en_US',
+        ],
+        [
+            'code'   => 'fr',
+            'name'   => 'Français',
+            'locale' => 'fr_FR',
+            'config' => [
+                'title'       => 'Cecil FR',
+                'description' => 'En français !',
+            ],
+        ],
+    ],
+    'virtualpages' => [
+        [
+            'path'   => '_redirects',
+            'output' => 'netlify_redirects',
+        ],
+        [
+            'path'      => 'rss',
+            'published' => false,
+        ],
+    ],
+    'defaultpages'    => [
+        'sitemap' => [
+            'published' => false,
+            'priority'  => 99,
+        ],
+    ],
+    'static' => [
+        'exclude' => [
+            'test*.txt',
+            '/\.php$/',
+        ],
+        'load' => true,
+    ],
+    'generators' => [
+        99  => 'Cecil\Generator\Test',
+        100 => 'Cecil\Generator\TitleReplace',
+    ],
+    'cache' => [
+        'enabled' => true,
+    ],
+];
