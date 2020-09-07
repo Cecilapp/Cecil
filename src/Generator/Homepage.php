71240099fcbaa960428c16e6c3b63bc83bfd2745
Arnaud Ligny <arnaud@ligny.org>
2020-09-08 00:34:44 +0200

    Fix virtual pages display on homepage

--- src/Generator/Homepage.php
+++ src/Generator/Homepage.php
@@ -25,7 +25,9 @@ class Homepage extends AbstractGenerator implements GeneratorInterface
     {
         $subPages = $this->builder->getPages()->filter(function (Page $page) {
             return $page->getType() == TYPE::PAGE
-                && $page->getId() != 'index'; // excludes homepage
+                && $page->getId() != 'index' // excludes homepage
+                && $page->isVirtual() === false // excludes virtual pages
+            ;
         });
         /** @var \Cecil\Collection\Page\Collection $subPages */
         $pages = $subPages->sortByDate();
