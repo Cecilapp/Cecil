659fe00dfa3bfdd80f9c623c55c9e014701cba33
Arnaud Ligny <arnaud@ligny.org>
2020-11-25 01:29:20 +0100

    Delete the Progress Bar code (#934)

--- src/Logger/ConsoleLogger.php
+++ src/Logger/ConsoleLogger.php
@@ -13,7 +13,6 @@ namespace Cecil\Logger;
 use Psr\Log\InvalidArgumentException;
 use Psr\Log\LogLevel;
 use Symfony\Component\Console\Formatter\OutputFormatterStyle;
-use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class ConsoleLogger extends PrintLogger
@@ -46,11 +45,6 @@ class ConsoleLogger extends PrintLogger
         LogLevel::DEBUG     => self::DEBUG,
     ];
 
-    /** @var ProgressBar */
-    protected $progressBar = null;
-    /** @var int */
-    private $progressBarMax;
-
     public function __construct(OutputInterface $output, array $verbosityLevelMap = [], array $formatLevelMap = [])
     {
         $this->output = $output;
@@ -69,26 +63,9 @@ class ConsoleLogger extends PrintLogger
         $output->getFormatter()->setStyle('text', new OutputFormatterStyle('white'));
         $output->getFormatter()->setStyle('debug', new OutputFormatterStyle('blue', 'yellow'));
 
-        // updates the levels mapping if output supports the Progress Bar
-        /* if ($output->isDecorated()) {
-            array_replace_recursive($this->verbosityLevelMap, [
-                LogLevel::NOTICE    => OutputInterface::VERBOSITY_NORMAL,
-                LogLevel::INFO      => OutputInterface::VERBOSITY_VERBOSE,
-            ]);
-        } */
-
         if (!isset($this->verbosityLevelMap[$level])) {
             throw new InvalidArgumentException(sprintf('The log level "%s" does not exist.', $level));
         }
-
-        // steps Progress Bar
-        /* if ($output->isDecorated()) {
-            if ($output->getVerbosity() == OutputInterface::VERBOSITY_NORMAL && array_key_exists('step', $context)) {
-                $this->printProgressBar($context['step'][0], $context['step'][1]);
-
-                return;
-            }
-        } */
 
         // default pattern: <level>message</level>
         $pattern = '<%1$s>%2$s%3$s</%1$s>';
@@ -101,14 +78,6 @@ class ConsoleLogger extends PrintLogger
 
         // sub steps progress
         if (array_key_exists('progress', $context)) {
-            // the verbose Progress Bar
-            /* if ($output->isDecorated()) {
-                if ($output->getVerbosity() == OutputInterface::VERBOSITY_VERBOSE) {
-                    $this->printProgressBar($context['progress'][0], $context['progress'][1]);
-
-                    return;
-                }
-            } */
             // prefix
             $prefix = sprintf(
                 '[%s/%s] ',
@@ -121,48 +90,6 @@ class ConsoleLogger extends PrintLogger
             sprintf($pattern, $this->formatLevelMap[$level], $prefix, $this->interpolate($message, $context)),
             $this->verbosityLevelMap[$level]
         );
-    }
-
-    /**
-     * Prints the Progress Bar.
-     *
-     * @param int $itemsCount
-     * @param int $itemsMax
-     *
-     * @return void
-     */
-    protected function printProgressBar(int $itemsCount, int $itemsMax): void
-    {
-        $this->createProgressBar($itemsMax);
-        $this->progressBar->clear();
-        $this->progressBar->setProgress($itemsCount);
-        $this->progressBar->display();
-        if ($itemsCount == $itemsMax) {
-            $this->progressBar->finish();
-            $this->output->writeln('');
-        }
-    }
-
-    /**
-     * Creates the Progress bar.
-     *
-     * @param int $max
-     *
-     * @return void
-     */
-    private function createProgressBar(int $max): void
-    {
-        if ($this->progressBar === null || $max != $this->progressBarMax) {
-            $this->progressBarMax = $max;
-            $this->progressBar = new ProgressBar($this->output, $max);
-            $this->progressBar->setOverwrite(true);
-            $this->progressBar->setFormat('%percent:3s%% [%bar%] %current%/%max%');
-            $this->progressBar->setBarCharacter('#');
-            $this->progressBar->setEmptyBarCharacter(' ');
-            $this->progressBar->setProgressCharacter('#');
-            $this->progressBar->setRedrawFrequency(1);
-            $this->progressBar->start();
-        }
     }
 
     /**
