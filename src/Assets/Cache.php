4d93858ae66ad21238e87f0b6a6e5a94cec52ca7
Arnaud Ligny <arnaud@ligny.org>
2021-05-06 00:14:34 +0200

    Clean code

--- src/Assets/Cache.php
+++ src/Assets/Cache.php
@@ -164,17 +164,17 @@ class Cache implements CacheInterface
     }
 
     /**
-     * Creates a cache key from a file (with MD5 hash).
+     * Creates a cache key from a file path (with MD5 hash).
      *
-     * @param string $file
      * @param string $path
+     * @param string $relativePath
      *
      * @return string
      */
-    public function createKeyFromFile(string $file, string $path): string
+    public function createKeyFromFile(string $path, string $relativePath): string
     {
-        $content = file_get_contents($file);
-        $key = $this->prepareKey(\sprintf('%s__%s', $path, $content !== false ? $this->createKeyFromValue($content) : ''));
+        $content = file_get_contents($path);
+        $key = $this->prepareKey(\sprintf('%s__%s', $relativePath, $content !== false ? $this->createKeyFromValue($content) : ''));
 
         return $key;
     }
@@ -194,7 +194,7 @@ class Cache implements CacheInterface
     }
 
     /**
-     * Returns cache file pathname.
+     * Returns cache file pathname from key.
      *
      * @param string $key
      *
