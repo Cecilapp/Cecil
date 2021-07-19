0c8d6bd1c2e2b8a2e2368b1b6f3f8fe26518af23
Arnaud Ligny <arnaud@ligny.org>
2021-07-19 02:27:20 +0200

    Fix feeds (#1124)

--- src/Collection/Taxonomy/Collection.php
+++ src/Collection/Taxonomy/Collection.php
@@ -11,25 +11,10 @@
 namespace Cecil\Collection\Taxonomy;
 
 use Cecil\Collection\Collection as CecilCollection;
-use Cecil\Collection\ItemInterface;
 
 /**
  * Class Collection.
  */
 class Collection extends CecilCollection
 {
-    /**
-     * Returns a Vocabulary collection (creates it if not exists).
-     * {@inheritdoc}
-     */
-    /*
-    public function get(string $id): ItemInterface
-    {
-        if (!$this->has($id)) {
-            $this->add(new Vocabulary($id));
-        }
-
-        return parent::get($id);
-    }
-    */
 }
