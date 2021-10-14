0808d23a3550afebc965f99f2bf23fb9d056d405
Arnaud Ligny <arnaud@ligny.org>
2021-10-14 20:13:30 +0200

    fix: json-ld config

--- tests/fixtures/website/config.php
+++ tests/fixtures/website/config.php
@@ -132,6 +132,9 @@ return [
         ],
     ],
     'metatags' => [
-        'jsonld' => true,
+        'jsonld' => [
+            'enabled'  => true,
+            'articles' => 'blog',
+        ]
     ],
 ];
