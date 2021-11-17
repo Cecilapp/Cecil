4199171ae338836b0c2d8316ad8db16874d458ac
Arnaud Ligny <arnaud@ligny.org>
2021-11-17 03:09:36 +0100

    Update Section.php

--- src/Generator/Section.php
+++ src/Generator/Section.php
@@ -83,7 +83,7 @@ class Section extends AbstractGenerator implements GeneratorInterface
                         $pages = $pages->$sortMethod();
                     }
                     // adds navigation links (excludes taxonomy pages)
-                    if (!in_array($page->getId(), array_keys($this->config->get('taxonomies')))) {
+                    if (!in_array($page->getId(), array_keys((array) $this->config->get('taxonomies')))) {
                         $this->addNavigationLinks($pages, $page->getVariable('sortby'), $page->getVariable('circular'));
                     }
                     // creates page for each section
