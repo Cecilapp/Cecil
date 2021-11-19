bd7dfac140ce39ed1e3da4e120f30c7b77d9aa51
Arnaud Ligny <arnaud@ligny.org>
2021-11-19 03:23:52 +0100

    some fix

--- src/Converter/Parsedown.php
+++ src/Converter/Parsedown.php
@@ -39,8 +39,8 @@ class Parsedown extends \ParsedownToC
         $image['element']['attributes']['src'] = $imageSource = trim($this->removeQuery($image['element']['attributes']['src']));
         $asset = new Asset($this->builder, $imageSource);
         $image['element']['attributes']['src'] = $asset;
-        if ($asset->getWidth() !== false) {
-            $width = $asset->getWidth();
+        if ($width = $asset->getWidth() !== false) {
+            return $image;
         }
         /**
          * Should be lazy loaded?
@@ -122,7 +122,7 @@ class Parsedown extends \ParsedownToC
         if (isset($classes)) {
             $Data['class'] = implode(' ', $classes);
         }
-        if (isset($HtmlAtt)) {
+        if (!empty($HtmlAtt)) {
             foreach ($HtmlAtt as $a => $v) {
                 $Data[$a] = trim($v, '"');
             }
