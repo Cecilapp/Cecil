9ef03b00aa8396ead2c219e11c2b62c9910e71d2
Arnaud Ligny <arnaud@ligny.org>
2022-09-06 09:27:41 +0200

    feat: support of i18n for homepage

--- src/Generator/Homepage.php
+++ src/Generator/Homepage.php
@@ -27,45 +27,53 @@ class Homepage extends AbstractGenerator implements GeneratorInterface
      */
     public function generate(): void
     {
-        // creates a new index page...
-        $page = (new Page('index'))->setPath('')->setVariable('title', 'Home');
-        // ... clones it if already exists
-        if ($this->builder->getPages()->has('index')) {
-            $page = clone $this->builder->getPages()->get('index');
-        }
-        // collects all pages
-        $subPages = $this->builder->getPages()->filter(function (Page $page) {
-            return $page->getType() == TYPE::PAGE
-                && $page->isVirtual() === false // excludes virtual pages
-                && $page->getVariable('exclude') !== true;
-        });
-        /** @var \Cecil\Collection\Page\Page $page */
-        if ($page->hasVariable('pagesfrom') && $this->builder->getPages()->has((string) $page->getVariable('pagesfrom'))) {
-            $subPages = $this->builder->getPages()->get((string) $page->getVariable('pagesfrom'))->getVariable('pages');
-        }
-        // sorts
-        /** @var \Cecil\Collection\Page\Collection $subPages */
-        /** @var \Cecil\Collection\Page\Page $page */
-        $pages = $subPages->sortByDate();
-        if ($page->hasVariable('sortby')) {
-            $sortMethod = \sprintf('sortBy%s', ucfirst((string) $page->getVariable('sortby')));
-            if (!method_exists($pages, $sortMethod)) {
-                throw new RuntimeException(\sprintf('In "%s" section "%s" is not a valid value for "sortby" variable.', $page->getId(), $page->getVariable('sortby')));
+        foreach ($this->config->getLanguages() as $lang) {
+            $language = $lang['code'];
+
+            $pageId = 'index';
+            if ($language != $this->config->getLanguageDefault()) {
+                $pageId = \sprintf('index.%s', $language);
             }
-            $pages = $pages->$sortMethod();
+            // creates a new index page...
+            $page = (new Page($pageId))->setPath('')->setVariable('title', 'Home');
+            // ... clones it if already exists
+            if ($this->builder->getPages()->has($pageId)) {
+                $page = clone $this->builder->getPages()->get($pageId);
+            }
+            // collects all pages
+            $subPages = $this->builder->getPages()->filter(function (Page $page) use ($language) {
+                return $page->getType() == TYPE::PAGE
+                    && $page->isVirtual() === false
+                    && $page->getVariable('exclude') !== true
+                    && $page->getVariable('language') == $language;
+            });
+
+            /** @var \Cecil\Collection\Page\Page $page */
+            if ($page->hasVariable('pagesfrom') && $this->builder->getPages()->has((string) $page->getVariable('pagesfrom'))) {
+                $subPages = $this->builder->getPages()->get((string) $page->getVariable('pagesfrom'))->getVariable('pages');
+            }
+            // sorts
+            /** @var \Cecil\Collection\Page\Collection $subPages */
+            /** @var \Cecil\Collection\Page\Page $page */
+            $pages = $subPages->sortByDate();
+            if ($page->hasVariable('sortby')) {
+                $sortMethod = \sprintf('sortBy%s', ucfirst((string) $page->getVariable('sortby')));
+                if (!method_exists($pages, $sortMethod)) {
+                    throw new RuntimeException(\sprintf('In page "%s" "%s" is not a valid value for "sortby" variable.', $page->getId(), $page->getVariable('sortby')));
+                }
+                $pages = $pages->$sortMethod();
+            }
+            /** @var \Cecil\Collection\Page\Page $page */
+            $page->setType(Type::HOMEPAGE)
+                ->setVariable('pages', $pages);
+            if ($pages->first()) {
+                $page->setVariable('date', $pages->first()->getVariable('date'));
+            }
+            // default menu
+            if (!$page->getVariable('menu')) {
+                $page->setVariable('menu', ['main' => ['weight' => 0]]);
+            }
+            $this->generatedPages->add($page);
         }
-        /** @var \Cecil\Collection\Page\Page $page */
-        $page->setType(Type::HOMEPAGE)
-            ->setVariable('pages', $pages);
-        if ($pages->first()) {
-            $page->setVariable('date', $pages->first()->getVariable('date'));
-        }
-        // default menu
-        if (!$page->getVariable('menu')) {
-            $page->setVariable('menu', [
-                'main' => ['weight' => 0],
-            ]);
-        }
-        $this->generatedPages->add($page);
     }
 }
