a3efd075580bf33b445b3a6674f679cfa3c77dcc
Arnaud Ligny <arnaud@ligny.org>
2022-10-03 00:44:42 +0200

    Update Converter.php

--- src/Converter/Converter.php
+++ src/Converter/Converter.php
@@ -40,57 +40,12 @@ class Converter implements ConverterInterface
      */
     public function convertFrontmatter(string $string, string $format = 'yaml'): array
     {
-        switch ($format) {
-            // https://wikipedia.org/wiki/INI_file
-            case 'ini':
-                $result = parse_ini_string($string, true);
-                if ($result === false) {
-                    throw new RuntimeException('Can\'t parse INI front matter.');
-                }
-
-                return $result;
-            // https://wikipedia.org/wiki/JSON
-            case 'json':
-                try {
-                    $result = json_decode($string, true);
-                    if ($result === null && json_last_error() !== JSON_ERROR_NONE) {
-                        throw new \Exception('JSON error.');
-                    }
-                } catch (\Exception $e) {
-                    throw new RuntimeException('Can\'t parse JSON front matter.');
-                }
-
-                return $result;
-            // https://wikipedia.org/wiki/TOML
-            case 'toml':
-                try {
-                    $result = Toml::Parse((string) $string) ?? [];
-                    if (!is_array($result)) {
-                        throw new RuntimeException('Can\'t parse TOML front matter.');
-                    }
-
-                    return $result;
-                } catch (TomlParseException $e) {
-                    throw new RuntimeException($e->getMessage(), $e->getParsedFile(), $e->getParsedLine());
-                } catch (\Exception $e) {
-                    throw new RuntimeException($e->getMessage());
-                }
-            // https://wikipedia.org/wiki/YAML
-            case 'yaml':
-            default:
-                try {
-                    $result = Yaml::parse((string) $string) ?? [];
-                    if (!is_array($result)) {
-                        throw new RuntimeException('Can\'t parse YAML front matter.');
-                    }
-
-                    return $result;
-                } catch (ParseException $e) {
-                    throw new RuntimeException($e->getMessage(), $e->getParsedFile(), $e->getParsedLine());
-                } catch (\Exception $e) {
-                    throw new RuntimeException($e->getMessage());
-                }
+        if (!in_array($format, ['yaml', 'ini', 'toml', 'json'])) {
+            throw new RuntimeException(\sprintf('The front matter format "%s" is not supported ("yaml", "ini", "toml" or "json").', $format));
         }
+        $method = \sprintf('convert%sToArray', ucfirst($format));
+
+        return self::$method($string);
     }
 
     /**
@@ -101,5 +56,79 @@ class Converter implements ConverterInterface
         $parsedown = new Parsedown($this->builder);
 
         return $parsedown->text($string);
+    }
+
+    /**
+     * Converts YAML string to array.
+     *
+     * @see https://wikipedia.org/wiki/YAML
+     */
+    private static function convertYamlToArray(string $string): array
+    {
+        try {
+            $result = Yaml::parse((string) $string) ?? [];
+            if (!is_array($result)) {
+                throw new RuntimeException('Can\'t parse YAML front matter.');
+            }
+
+            return $result;
+        } catch (ParseException $e) {
+            throw new RuntimeException($e->getMessage(), $e->getParsedFile(), $e->getParsedLine());
+        } catch (\Exception $e) {
+            throw new RuntimeException($e->getMessage());
+        }
+    }
+
+    /**
+     * Converts INI string to array.
+     *
+     * @see https://wikipedia.org/wiki/INI_file
+     */
+    private static function convertIniToArray(string $string): array
+    {
+        $result = parse_ini_string($string, true);
+        if ($result === false) {
+            throw new RuntimeException('Can\'t parse INI front matter.');
+        }
+
+        return $result;
+    }
+
+    /**
+     * Converts TOML string to array.
+     *
+     * @see https://wikipedia.org/wiki/TOML
+     */
+    private static function convertTomlToArray(string $string): array
+    {
+        try {
+            $result = Toml::Parse((string) $string) ?? [];
+            if (!is_array($result)) {
+                throw new RuntimeException('Can\'t parse TOML front matter.');
+            }
+
+            return $result;
+        } catch (TomlParseException $e) {
+            throw new RuntimeException($e->getMessage(), $e->getParsedFile(), $e->getParsedLine());
+        } catch (\Exception $e) {
+            throw new RuntimeException($e->getMessage());
+        }
+    }
+
+    /**
+     * Converts JSON string to array.
+     *
+     * @see https://wikipedia.org/wiki/JSON
+     */
+    private static function convertJsonToArray(string $string): array
+    {
+        try {
+            $result = json_decode($string, true);
+            if ($result === null && json_last_error() !== JSON_ERROR_NONE) {
+                throw new \Exception('JSON error.');
+            }
+        } catch (\Exception $e) {
+            throw new RuntimeException('Can\'t parse JSON front matter.');
+        }
     }
 }
