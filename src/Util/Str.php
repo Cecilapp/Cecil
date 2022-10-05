87d2aa6ff8d34990ec1d4bac9085fb3629455fe0
Arnaud Ligny <arnaud@ligny.org>
2022-10-05 22:39:47 +0200

    refactor: clean code

--- src/Util/Str.php
+++ src/Util/Str.php
@@ -16,22 +16,6 @@ namespace Cecil\Util;
 class Str
 {
     /**
-     * Converts an array to a string.
-     *
-     * ie: [0 => 'A', 1 => 'B'] become '0:A, 1:B'
-     */
-    public static function arrayToString(array $array, string $separator = ':'): string
-    {
-        $string = '';
-
-        foreach ($array as $key => $value) {
-            $string .= \sprintf('%s%s%s, ', $key, $separator, $value);
-        }
-
-        return substr($string, 0, -2);
-    }
-
-    /**
      * Combines an array into a string.
      *
      * @param string $keyToKey   The key that become the key of the new array
