0c06c6a297eba89865b066482adc698541eabe80
Arnaud Ligny <arnaud@ligny.fr>
2023-04-19 14:36:00 +0200

    Update Page.php

--- src/Renderer/Page.php
+++ src/Renderer/Page.php
@@ -14,17 +14,16 @@ declare(strict_types=1);
 namespace Cecil\Renderer;
 
 use Cecil\Collection\Page\Page as PageItem;
-use Cecil\Config;
 
 /**
  * Class Renderer\Page.
  */
 class Page
 {
-    /** @var Config */
+    /** @var \Cecil\Config */
     protected $config;
 
-    public function __construct(Config $config)
+    public function __construct(\Cecil\Config $config)
     {
         $this->config = $config;
     }
