2c546f7d8bda27b0953e2a96f287370d29a12b63
Arnaud Ligny <arnaud@ligny.org>
2023-12-08 15:18:11 +0100

    Update cecil-build.sh

--- scripts/cecil-build.sh
+++ scripts/cecil-build.sh
@@ -2,11 +2,11 @@
 
 # This script build a Cecil website (locally, on Netlify / Vercel / Cloudflare Pages / Render).
 # It is intended to be used on CI / CD.
-export PHP_MIN_VERSION="7.4"
+export PHP_MIN_VERSION="8.1"
 
 # Default variables
 if [ -z "${PHP_VERSION}" ]; then
-  export PHP_VERSION="7.4"
+  export PHP_VERSION="8.1"
 fi
 if [ -z "${CECIL_INSTALL_OPTIM}" ]; then
   export CECIL_INSTALL_OPTIM="false"
