90404ad69e56bb84a2563e9d820cffc341864862
Arnaud Ligny <arnaud@ligny.fr>
2025-03-08 02:54:42 +0100

    Update CacheClearTemplates.php

--- src/Command/CacheClearTemplates.php
+++ src/Command/CacheClearTemplates.php
@@ -16,8 +16,8 @@ namespace Cecil\Command;
 use Cecil\Util;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
 
 /**
  * Removes templates cache files.
