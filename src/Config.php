5727f82329a7a631a814a4c158b327c3bb67f9cc
Arnaud Ligny <arnaud@ligny.fr>
2025-03-26 02:48:18 +0100

    Update Config.php

--- src/Config.php
+++ src/Config.php
@@ -143,11 +143,11 @@ class Config
      */
     public function isEnabled(string $key, ?string $language = null, bool $fallback = true): bool
     {
-        if ($this->has("$key.enabled", $language, $fallback) && $this->get("$key.enabled", $language, $fallback) === false) {
-            return false;
-        }
         if ($this->has($key, $language, $fallback) && $this->get($key, $language, $fallback) !== false) {
             return true;
+        }
+        if ($this->has("$key.enabled", $language, $fallback) && $this->get("$key.enabled", $language, $fallback) === false) {
+            return false;
         }
 
         return false;
