56c4c11105917815a0bcdcfd6a93e9a9d742a268
Arnaud Ligny <arnaud@ligny.fr>
2025-04-02 00:52:58 +0200

    Update Parsedown.php

--- src/Converter/Parsedown.php
+++ src/Converter/Parsedown.php
@@ -675,7 +675,7 @@ class Parsedown extends \ParsedownToc
                 if (isset($block['element']['attributes']['poster'])) {
                     $block['element']['attributes']['poster'] = (string) new Asset($this->builder, $block['element']['attributes']['poster'], ['force_slash' => false]);
                 }
-                $block['element']['attributes']['style'] = 'background-color: #d8d8d8;'; // background color if offline
+                $block['element']['attributes']['style'] .= ';max-width:100%%;height:auto;background-color: #d8d8d8;'; // background color if offline
 
                 return $block;
             case 'audio':
