33c0f22605721e8f9d66e13de5f24bc6ebcc0f4a
Arnaud Ligny <arnaud@ligny.fr>
2025-04-10 17:03:50 +0200

    Update AbstractCommand.php

--- src/Command/AbstractCommand.php
+++ src/Command/AbstractCommand.php
@@ -110,7 +110,19 @@ class AbstractCommand extends Command
             if ($this->io === null) {
                 $this->io = new SymfonyStyle($input, $output);
             }
-            $this->io->error($e->getMessage());
+            $i = 0;
+            $message = '';
+            do {
+                if ($e instanceof \Twig\Error\RuntimeError ) {
+                    continue;
+                }
+                $i++;
+                $message .= "$i. {$e->getMessage()}\n";
+                if ($e->getFile()) {
+                    $message .= \sprintf("   File: %s%s\n", $e->getFile(), $e->getLine() ? ":{$e->getLine()}" : '');
+                }
+            } while($e = $e->getPrevious());
+            $this->io->error($message);
 
             exit(1);
         }
