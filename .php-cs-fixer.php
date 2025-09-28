55b003bed27c4ff094311a3be2ebe3493a498a8f
Arnaud Ligny <arnaud@ligny.fr>
2025-09-28 02:23:10 +0200

    chore: update PHP CS fixer

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -9,31 +9,39 @@
  * file that was distributed with this source code.
  */
 
-$finder = (new PhpCsFixer\Finder())
-    ->ignoreDotFiles(false)
-    ->ignoreVCSIgnored(true)
-    ->exclude(['tests/fixtures'])
-    ->in(__DIR__)
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
+
+$fileHeader =
+    <<<'EOF'
+    This file is part of Cecil.
+
+    (c) Arnaud Ligny <arnaud@ligny.fr>
+
+    For the full copyright and license information, please view the LICENSE
+    file that was distributed with this source code.
+    EOF
 ;
 
-return (new PhpCsFixer\Config())
-    ->setParallelConfig(PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect())
+return (new Config())
+    ->setParallelConfig(ParallelConfigFactory::detect())
+    ->setUnsupportedPhpVersionAllowed(true)
     ->setRules([
         '@PSR12' => true,
         'native_function_invocation' => true,
         'nullable_type_declaration_for_default_null_value' => true,
         'header_comment' => [
-            'header' => <<<'EOF'
-            This file is part of Cecil.
-
-            (c) Arnaud Ligny <arnaud@ligny.fr>
-
-            For the full copyright and license information, please view the LICENSE
-            file that was distributed with this source code.
-            EOF,
+            'header' => $fileHeader,
             'comment_type' => 'PHPDoc',
             'location' => 'after_open',
         ],
     ])
-    ->setFinder($finder)
+    ->setFinder(
+        (new Finder())
+            ->ignoreDotFiles(false)
+            ->ignoreVCSIgnored(true)
+            ->exclude(['tests/fixtures'])
+            ->in(__DIR__)
+    )
 ;
