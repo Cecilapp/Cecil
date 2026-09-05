4eddfdfa1bf457f69b70976295653de54a20bdb4
Arnaud Ligny <arnaud@ligny.fr>
2026-09-05 16:43:49 +0200

    Add license header to Twig CS fixer

--- .twig-cs-fixer.php
+++ .twig-cs-fixer.php
@@ -1,4 +1,14 @@
 <?php
+
+/**
+ * This file is part of Cecil.
+ *
+ * (c) Arnaud Ligny <arnaud@ligny.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 $ruleset = new TwigCsFixer\Ruleset\Ruleset();
 $ruleset->addStandard(new TwigCsFixer\Standard\TwigCsFixer());
 $ruleset->removeRule(TwigCsFixer\Rules\Whitespace\BlankEOFRule::class);
